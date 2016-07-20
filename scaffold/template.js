d2bfdb9aec5ec698efd376e84e5e8f9c0c274602
tongjinle <1328192478@qq.com>
2016-07-20 17:36:22 +0800

    完成脚手架

--- scaffold/template.js
+++ scaffold/template.js
@@ -1,55 +1,39 @@
 var template = {
 	controller:
 	`
-/// <reference path="../../typings/index.d.ts" />
+/// <reference path="../typings/index.d.ts" />
 
-define(['app'],(app:angular.IModule)=>{
-	app.controller('<%= ctrlName %>Ctrl',['$scope',(scope)=>{
+define(['app'],function(app:angular.IModule){
+	app.controller('<%= ctrlName %>Ctrl',['$scope',function(scope:angular.IScope){
 
 	}]);
 });
-
 	`,
 	service:
 	`
-/// <reference path="../../typings/index.d.ts" />
+/// <reference path="../typings/index.d.ts" />
 
-define(['app'], (app: angular.IModule) => {
-	app.service('<%= serviceName %>Service', function() {
-
-		this.get = () => {
-			return 'typescript angular';
-		};
-	});
-});
-
-	`
-	,
-	'directive.js':
-	`
-define(['app'],function(app){
-	app.directive('<%= name %>',[function(){
-		return {
-			restrict:'E',
-			replace:false,
-			templateUrl:'../directive/html/<%= name %>.html',
-			link:function(scope,elements,attrs){
-				
-			}
+define(['app'],function(app:angular.IModule){
+	app.service('<%= serviceName %>Service',[function(){
+		this.version = function(){
+			return 'lingmall scaffolding version 0.1.0';
 		};
 	}]);
 });
-
+	`
+	,
+	'directive.ts':
+	`
 /// <reference path="../../typings/index.d.ts" />
 
-define(['app'], (app: angular.IModule) => {
-	app.directive('<%= name %>', [() => {
+define(['app'],function(app){
+	app.directive('<%= name %>',[function(){
 		let dire: angular.IDirective;
 		dire = {
 			restrict: 'E',
 			replace: false,
 			templateUrl: '/directive/html/<%= name %>.html',
-			link: (scope: any, el, attrs) => {
+			link: (scope, el, attrs) => {
 				
 			}
 		};
